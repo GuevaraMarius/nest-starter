9a8a79549e280a4a72df88d3094df07da7ebb90b
GuevaraMarius <irabizimguevara@gmail.com>
2024-08-13 14:23:34 +0200

    CRUD functionality for todo

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { TodoModule } from './todo/todo.module';
 
@@ -18,7 +16,5 @@ import { TodoModule } from './todo/todo.module';
     }),
     TodoModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
