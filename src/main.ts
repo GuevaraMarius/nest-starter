2bb0c772cf22f7834ca54393d73664fa6d6b1cc2
GuevaraMarius <irabizimguevara@gmail.com>
2024-08-21 20:55:57 +0200

    Feat:authentication & authorization

--- src/main.ts
+++ src/main.ts
@@ -4,21 +4,30 @@ import { ValidationPipe } from '@nestjs/common';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AllExceptionsFilter } from './todo/filters/all-exceptions.filter';
 import { config } from 'dotenv';
+
 config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  // Enable CORS with options
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
 
   // Set up global pipes and filters
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
   app.useGlobalFilters(new AllExceptionsFilter());
 
-  // Swagger setup
+  // Swagger setup with auth
   const config = new DocumentBuilder()
     .setTitle('Todo API')
     .setDescription('The Todo API description')
     .setVersion('1.0')
+    .addBearerAuth()
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -10,19 +10,31 @@ import {
   UsePipes,
   UseFilters,
   ValidationPipe,
+  UseGuards,
+  Req,
+  ForbiddenException,
 } from '@nestjs/common';
 import { TodoService } from './todo.service';
 import { CreateTodoDto, UpdateTodoDto } from './dto/todos-dto';
 import { AllExceptionsFilter } from './filters/all-exceptions.filter';
 import { Todo } from './todo.entity';
 import { ResponseDto } from 'src/shared/response-dto';
+import { RolesGuard } from 'src/auth/guard/roles.guard';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { AuthGuard } from '@nestjs/passport';
+import { ERole } from 'src/user/role.enum';
+import { Roles } from 'src/auth/roles.decorator';
 
+@UseGuards(AuthGuard('jwt'), RolesGuard)
+@ApiTags('Todos')
+@ApiBearerAuth()
 @Controller('todos')
 @UseFilters(AllExceptionsFilter)
 export class TodoController {
   constructor(private readonly todoService: TodoService) {}
 
   @Get()
+  @Roles(ERole.ADMIN)
   async findAll(
     @Query('page') page?: number,
     @Query('limit') limit?: number,
@@ -37,12 +49,46 @@ export class TodoController {
     );
   }
 
+  @Get('my')
+  @Roles(ERole.USER)
+  async findMyTodos(
+    @Req() req: any,
+    @Query('page') page?: number,
+    @Query('limit') limit?: number,
+    @Query('isDone') isDone?: boolean,
+  ): Promise<ResponseDto<Todo[]>> {
+    const userId = req.user.userId;
+    const todos = await this.todoService.findUserTodos(
+      userId,
+      page,
+      limit,
+      isDone,
+    );
+    return new ResponseDto(
+      200,
+      'success',
+      'Your todos retrieved successfully',
+      todos,
+    );
+  }
+
   @Get(':id')
-  async findOne(@Param('id') id: number): Promise<ResponseDto<Todo>> {
+  @Roles(ERole.USER, ERole.ADMIN)
+  async findOne(
+    @Req() req: any,
+    @Param('id') id: number,
+  ): Promise<ResponseDto<Todo>> {
     const todo = await this.todoService.findOne(id);
+    const userId = req.user.userId;
+
     if (!todo) {
       return new ResponseDto(404, 'error', `Todo with ID ${id} not found`);
     }
+
+    if (req.user.role === ERole.USER && todo.user.id !== userId) {
+      throw new ForbiddenException('You are not allowed to view this todo');
+    }
+
     return new ResponseDto(
       200,
       'success',
@@ -52,11 +98,14 @@ export class TodoController {
   }
 
   @Post()
+  @Roles(ERole.USER)
   @UsePipes(new ValidationPipe({ transform: true }))
   async create(
+    @Req() req: any,
     @Body() createTodoDto: CreateTodoDto,
   ): Promise<ResponseDto<Todo>> {
-    const newTodo = await this.todoService.create(createTodoDto);
+    const userId = req.user.userId;
+    const newTodo = await this.todoService.create(createTodoDto, userId);
     return new ResponseDto(
       201,
       'success',
@@ -66,22 +115,35 @@ export class TodoController {
   }
 
   @Put(':id')
+  @Roles(ERole.USER)
   @UsePipes(new ValidationPipe({ transform: true }))
   async update(
+    @Req() req: any,
     @Param('id') id: number,
     @Body() updateTodoDto: UpdateTodoDto,
   ): Promise<ResponseDto<Todo>> {
-    await this.todoService.update(id, updateTodoDto);
+    const userId = req.user.userId;
+    const updatedTodo = await this.todoService.update(
+      id,
+      updateTodoDto,
+      userId,
+    );
     return new ResponseDto(
       200,
       'success',
       `Todo with ID ${id} updated successfully`,
+      updatedTodo,
     );
   }
 
   @Delete(':id')
-  async remove(@Param('id') id: number): Promise<ResponseDto<void>> {
-    await this.todoService.remove(id);
+  @Roles(ERole.USER)
+  async remove(
+    @Req() req: any,
+    @Param('id') id: number,
+  ): Promise<ResponseDto<void>> {
+    const userId = req.user.userId;
+    await this.todoService.remove(id, userId);
     return new ResponseDto(
       200,
       'success',
